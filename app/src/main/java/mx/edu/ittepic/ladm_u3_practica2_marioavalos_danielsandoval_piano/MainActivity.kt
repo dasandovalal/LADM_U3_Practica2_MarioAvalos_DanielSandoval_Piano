df3895e7b5bf098f2800dff6d8864b0234c60aca
Dany Sandoval <dasandovalal@ittepic.edu.mx>
2022-11-10 16:59:20 -0700

    Proyecto vacio

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.ittepic.ladm_u3_practica2_marioavalos_danielsandoval_piano
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
