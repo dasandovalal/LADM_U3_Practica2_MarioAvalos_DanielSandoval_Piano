3a3c9e33c1a6990fed06da980547ba7204bd23c5
Dany Sandoval <dasandovalal@ittepic.edu.mx>
2022-11-11 16:28:04 -0700

    Solo dibujado DO y RE mas carpeta RAW

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/Lienzo.kt
@@ -0,0 +1,44 @@
+package mx.edu.ittepic.ladm_u3_practica2_marioavalos_danielsandoval_piano
+
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.View
+
+class Lienzo(activity: Activity):View(activity) {
+
+    val p = Paint()
+
+    override fun onDraw(c: Canvas) {
+        super.onDraw(c)
+
+        c.drawColor(Color.YELLOW)
+
+        //DO
+        p.color = Color.WHITE
+        c.drawRect(6f,6f,286f,1010f,p) //TECLA
+        p.color = Color.BLACK
+        p.style = Paint.Style.STROKE
+        p.strokeWidth = 6f
+        c.drawRect(3f,3f,289f,1013f,p) //CONTORNO
+        p.color = Color.RED
+        p.style = Paint.Style.FILL
+        p.textSize = 60f
+        c.drawText("DO",110f,950f,p) //TEXTO
+
+        //RE
+        p.color = Color.WHITE
+        c.drawRect(289f,6f,569f,1010f,p) //TECLA
+        p.color = Color.BLACK
+        p.style = Paint.Style.STROKE
+        p.strokeWidth = 6f
+        c.drawRect(292f,3f,572f,1013f,p) //CONTORNO
+        p.color = Color.RED
+        p.style = Paint.Style.FILL
+        p.textSize = 60f
+        c.drawText("RE",393f,950f,p) //TEXTO
+
+
+    }
+}
--- app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/MainActivity2.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/MainActivity2.kt
@@ -1,18 +1,14 @@
 package mx.edu.ittepic.ladm_u3_practica2_marioavalos_danielsandoval_piano
 
-import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(Lienzo(this))
         supportActionBar!!.hide() //Ocultar la barra del titulo
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE //Poner la pantalla horizontal
-
-        startActivity(Intent(this,MainActivity2::class.java))
     }
 }
