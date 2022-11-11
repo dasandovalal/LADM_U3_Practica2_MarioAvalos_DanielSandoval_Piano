96c09710dcb3fe71b021c9300ce60902babaf9e4
Dany Sandoval <dasandovalal@ittepic.edu.mx>
2022-11-10 17:27:58 -0700

    Pantalla horizontal

--- app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/MainActivity.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/MainActivity.kt
@@ -1,5 +1,6 @@
 package mx.edu.ittepic.ladm_u3_practica2_marioavalos_danielsandoval_piano
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE //Poner la pantalla horizontal
     }
 }
