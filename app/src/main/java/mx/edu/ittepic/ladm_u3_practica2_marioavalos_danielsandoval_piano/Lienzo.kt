43f74de2a07788ab8aa68a6c72d17a1f435c1bd8
Dany Sandoval <dasandovalal@ittepic.edu.mx>
2022-11-12 20:22:46 -0700

    Funcion de sonido

--- app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/Lienzo.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/Lienzo.kt
@@ -4,15 +4,20 @@ import android.app.Activity
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.media.MediaPlayer
+import android.view.MotionEvent
 import android.view.View
 
 class Lienzo(activity: Activity):View(activity) {
+
+    val activity = activity
 
     val DO = Tecla(this,"DO",Color.WHITE,R.raw.c,6f,6f,286f,1010f)
     val RE = Tecla(this,"RE",Color.WHITE,R.raw.d,289f,6f,569f,1010f)
     val DO_S = Tecla(this,"DO#",Color.BLACK,R.raw.c_s,190f,6f,393f,720f)
 
     val P = Paint()
+    var teclaTocada : Tecla? = null
 
     override fun onDraw(c: Canvas) {
         super.onDraw(c)
@@ -46,5 +51,26 @@ class Lienzo(activity: Activity):View(activity) {
         c.drawText("DO#",247f,650f,p) //TEXTO*/
 
 
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN->{
+                //Saber qué tecla se presionó y sonar
+                if (DO_S.tocoTecla(event.x,event.y)) {
+                    DO_S.reproducirSonido(activity)
+                }
+                if (DO.tocoTecla(event.x,event.y)) {
+                    DO.reproducirSonido(activity)
+                }
+                if (RE.tocoTecla(event.x,event.y)) {
+                    RE.reproducirSonido(activity)
+                }
+
+            }
+        }
+
+        return true
     }
 }
--- app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/Tecla.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u3_practica2_marioavalos_danielsandoval_piano/Tecla.kt
@@ -1,8 +1,11 @@
 package mx.edu.ittepic.ladm_u3_practica2_marioavalos_danielsandoval_piano
 
+import android.app.Activity
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.media.MediaPlayer
+import android.view.MotionEvent
 
 class Tecla(lienzo: Lienzo, txtTecla:String, teclaColor:Int, sonido:Int,
     left:Float, top:Float, right:Float, bottom:Float) {
@@ -29,5 +32,24 @@ class Tecla(lienzo: Lienzo, txtTecla:String, teclaColor:Int, sonido:Int,
             c.drawText(TXT_TECLA,LEFT+57f,BOTTOM-70f,P)
         }
     }
+
+    fun tocoTecla(xToque: Float, yToque: Float):Boolean{
+        var bottomTOP = BOTTOM-290
+        if (xToque>=LEFT && xToque<=RIGHT){
+            if (yToque>=bottomTOP && yToque<=BOTTOM){
+                return true
+            }
+        }
+        return false
+    }
+
+    fun reproducirSonido(activity: Activity){
+        var sonido = MediaPlayer.create(activity,SONIDO)
+        sonido.seekTo(0)
+        sonido.start()
+
+    }
+
+
 
 }
